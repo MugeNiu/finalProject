490740049f31b94613d34311261916afdb301ee0
MugeNiu <mn2674@columbia.edu>
2015-05-03 22:58:17 -0400

    changed nav bar

--- js/main.js
+++ js/main.js
@@ -1,53 +1,58 @@
 var stepInfo_china = {
     step01: {
         name: 'register_business',
-        //text: 'These dogs are awesome and are my favorite.'
         stepNumber_arr: ["1","2","3","4"],
         details_arr: ["a","b","c","d"],
+        story_arr:["quote1","quote2","quote3","quote4"],
         day:[1,3,6,9],
         cost:[12,15,18,130]
     },
     step02: {
         name: 'Getting Electricity',
         stepNumber_arr: ["","","",""],
-        details_arr: ["","","",""]
+        details_arr: ["","","",""],
+        story_arr:["quote1","quote2","quote3","quote4"]
     },
     step03: {
         name: 'Registering Property',
         stepNumber_arr: ["","","",""],
-        details_arr: ["","","",""]
-    },
+        details_arr: ["","","",""],
+        story_arr:["quote1","quote2","quote3","quote4"]
+	},
     step04: {
         name: 'Paying Taxes',
         stepNumber_arr: ["","","",""],
-        details_arr: ["","","",""]
+        details_arr: ["","","",""],
+        story_arr:["quote1","quote2","quote3","quote4"]
     }
 };
 
 var stepInfo_us = {
     step01: {
         name: 'register_business',
-        //text: 'These dogs are awesome and are my favorite.'
-
         stepNumber_arr: ["1","2","3","4","5"],
         details_arr: ["a","b","c","d","e"],
+        story_arr:["quote1","quote2","quote3","quote4"],
         day:[1,3,6,9,14],
         cost:[12,15,18,130,150]
     },
     step02: {
         name: 'Getting Electricity',
         stepNumber_arr: ["","","",""],
-        details_arr: ["","","",""]
+        details_arr: ["","","",""],
+        story_arr:["quote1","quote2","quote3","quote4"]
     },
     step03: {
         name: 'Registering Property',
         stepNumber_arr: ["","","",""],
-        details_arr: ["","","",""]
+        details_arr: ["","","",""],
+ 		story_arr:["quote1","quote2","quote3","quote4"]
     },
     step04: {
         name: 'Paying Taxes',
         stepNumber_arr: ["","","",""],
-        details_arr: ["","","",""]
+        details_arr: ["","","",""],
+		story_arr:["quote1","quote2","quote3","quote4"]
     }
 };
 
@@ -64,23 +69,25 @@ var stepInfo_us = {
 
 $('.procedure').on('click', function(){
 	
-	var svg_id = $(this).attr('data-which');
-	//get the element of svg
-	var elem_svg = document.getElementById(svg_id);
-	$('.starchange').removeClass('active');
-	$(elem_svg).addClass('active');
-	
+	// var svg_id = $(this).attr('data-which');
+	// //get the element of svg
+	// var elem_svg = document.getElementById(svg_id);
+	// $('.starchange').removeClass('active');
+	// $(elem_svg).addClass('active');
+
 
 	var stepNumber = $(this).attr('data-key');
 	//for china part
 	//extract related info regarding stepNumber
 	var stepNumber_china = stepInfo_china[stepNumber].stepNumber_arr;
 	var details_china = stepInfo_china[stepNumber].details_arr;
+	var story_china = stepInfo_china[stepNumber].story_arr;
 	var time_china = stepInfo_china[stepNumber].day;
 	var cost_china = stepInfo_china[stepNumber].cost;
 
 	var stepNumber_us = stepInfo_us[stepNumber].stepNumber_arr;
 	var details_us = stepInfo_us[stepNumber].details_arr;
+	var story_us = stepInfo_us[stepNumber].story_arr;
 	var time_us = stepInfo_us[stepNumber].day;
 	var cost_us = stepInfo_us[stepNumber].cost;
 
@@ -88,10 +95,12 @@ $('.procedure').on('click', function(){
 	var div_stepNumber_china = document.getElementById("stepName China");
 	var div_detail_china = document.getElementById("details China");
 	var div_day_china = document.getElementById("dayChina");
+	var div_story_china= document.getElementById("story China");
 	var div_cost_china = document.getElementById("RMB");
 
 	var div_stepNumber_us = document.getElementById("stepName us");
 	var div_detail_us = document.getElementById("details us");
+	var div_story_us= document.getElementById("story US");
 	var div_day_us = document.getElementById("dayUS");
 	var div_cost_us = document.getElementById("dollar");
 
@@ -99,11 +108,13 @@ $('.procedure').on('click', function(){
 	//default to show the first small step
 	$(div_stepNumber_china).html(stepNumber_china[0]);
 	$(div_detail_china).html(details_china[0]);
+	$(div_story_china).html(story_china[0]);
 	$(div_day_china).html(time_china[0]);
 	$(div_cost_china).html(cost_china[0]);
 
 	$(div_stepNumber_us).html(stepNumber_us[0]);
 	$(div_detail_us).html(details_us[0]);
+	$(div_story_us).html(story_us[0]);
 	$(div_day_us).html(time_us[0]);
 	$(div_cost_us).html(cost_us[0]);
 
@@ -117,6 +128,7 @@ $('.procedure').on('click', function(){
 		if(i < stepNumber_china.length){
 			$(div_stepNumber_china).html(stepNumber_china[i]);
 		 	$(div_detail_china).html(details_china[i]);
+		 	$(div_story_china).html(story_china[i]);
 		 	$(div_cost_china).html(cost_china[i]);
 		 	$(div_day_china).html(time_china[i]);
 		}
@@ -124,6 +136,7 @@ $('.procedure').on('click', function(){
 		if(i < stepNumber_us.length){
 		 	$(div_stepNumber_us).html(stepNumber_us[i]);
 		 	$(div_detail_us).html(details_us[i]);
+		 	$(div_story_us).html(story_us[i]);
 		 	$(div_cost_us).html(cost_us[i]);
 		 	$(div_day_us).html(time_us[i]);
 		}
@@ -136,7 +149,7 @@ $('.procedure').on('click', function(){
 
 	console.log("len="+len);
 	console.log(i);
-	},5000)
+	},1000)
 
 	
 })
